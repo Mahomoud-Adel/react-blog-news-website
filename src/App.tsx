531faa3f4feb2059ceb0d5b78c2ac9700fcd9f49
Mahmoud-Adel <mahmodadel2030@gmail.com>
2024-12-29 01:53:19 +0200

    install packages

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button"
+
+
+function App() {
+
+
+  return (
+    <Button>
+      <i className="fa-solid fa-user"></i>
+    </Button>
+  )
+}
+
+export default App
